75b86cd6ba9b1fdcfae3b8207f8ad7caada99e79
Kate Piette <kp1269@F16615.ad.messiah.edu>
2015-12-06 15:52:04 -0500

    n

--- Forecaster/Forecaster/Predictions1.vb
+++ Forecaster/Forecaster/Predictions1.vb
@@ -1,12 +1,17 @@
 ﻿
 Class Predictions
-    Shared Sub NaivePrediction()
-
-    End Sub
     Shared Sub ExponentialSmoothingPrediction()
 
     End Sub
-    Shared Sub WeightedMovingAveragePrediction()
-
+    Shared Sub WeightedMovingPrediction(Date)
+        Dim F As Integer = 0
+        Dim N As Integer = 10 'Number of Weeks
+        Dim D As Integer = 0
+        Do
+            D = !!!!!Day!!!!! * N
+            F = F + D
+            N = N - 1
+        Loop Until N > 1
+        F = F / (10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
     End Sub
 End Class
