81e47f53bf50094139ad32d53a81e2eb9750c57f
Andrew Cameron <cameronaj@verizon.net>
2015-12-06 15:18:26 -0500

    Database Update and Sale Add functions

---
+++ Forecaster/Forecaster/addSalesForm.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAddSales
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAddSales))
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.mskDate = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.OleDbDataAdapter1 = New System.Data.OleDb.OleDbDataAdapter()
+        Me.OleDbDeleteCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbInsertCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbSelectCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbUpdateCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbConnection1 = New System.Data.OleDb.OleDbConnection()
+        Me.SuspendLayout()
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Location = New System.Drawing.Point(55, 59)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(38, 17)
+        Me.lblDate.TabIndex = 0
+        Me.lblDate.Text = "Date"
+        '
+        'mskDate
+        '
+        Me.mskDate.Location = New System.Drawing.Point(117, 54)
+        Me.mskDate.Mask = "00/00/0000"
+        Me.mskDate.Name = "mskDate"
+        Me.mskDate.Size = New System.Drawing.Size(100, 22)
+        Me.mskDate.TabIndex = 1
+        Me.mskDate.ValidatingType = GetType(Date)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 107)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Amount"
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(117, 102)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(100, 22)
+        Me.txtAmount.TabIndex = 3
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(81, 163)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(100, 23)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add Record"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'OleDbDataAdapter1
+        '
+        Me.OleDbDataAdapter1.DeleteCommand = Me.OleDbDeleteCommand1
+        Me.OleDbDataAdapter1.InsertCommand = Me.OleDbInsertCommand1
+        Me.OleDbDataAdapter1.SelectCommand = Me.OleDbSelectCommand1
+        Me.OleDbDataAdapter1.TableMappings.AddRange(New System.Data.Common.DataTableMapping() {New System.Data.Common.DataTableMapping("Table", "Sales", New System.Data.Common.DataColumnMapping() {New System.Data.Common.DataColumnMapping("ID", "ID"), New System.Data.Common.DataColumnMapping("sYear", "sYear"), New System.Data.Common.DataColumnMapping("sWeek", "sWeek"), New System.Data.Common.DataColumnMapping("sDay", "sDay"), New System.Data.Common.DataColumnMapping("sAmount", "sAmount")})})
+        Me.OleDbDataAdapter1.UpdateCommand = Me.OleDbUpdateCommand1
+        '
+        'OleDbDeleteCommand1
+        '
+        Me.OleDbDeleteCommand1.CommandText = resources.GetString("OleDbDeleteCommand1.CommandText")
+        Me.OleDbDeleteCommand1.Parameters.AddRange(New System.Data.OleDb.OleDbParameter() {New System.Data.OleDb.OleDbParameter("Original_ID", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "ID", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_sYear", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "sYear", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_sYear", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "sYear", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_sWeek", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "sWeek", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_sWeek", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "sWeek", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_sDay", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "sDay", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_sDay", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "sDay", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_sAmount", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "sAmount", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_sAmount", System.Data.OleDb.OleDbType.Currency, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "sAmount", System.Data.DataRowVersion.Original, Nothing)})
+        '
+        'OleDbInsertCommand1
+        '
+        Me.OleDbInsertCommand1.CommandText = "INSERT INTO `Sales` (`sYear`, `sWeek`, `sDay`, `sAmount`) VALUES (?, ?, ?, ?)"
+        Me.OleDbInsertCommand1.Parameters.AddRange(New System.Data.OleDb.OleDbParameter() {New System.Data.OleDb.OleDbParameter("sYear", System.Data.OleDb.OleDbType.[Integer], 0, "sYear"), New System.Data.OleDb.OleDbParameter("sWeek", System.Data.OleDb.OleDbType.[Integer], 0, "sWeek"), New System.Data.OleDb.OleDbParameter("sDay", System.Data.OleDb.OleDbType.VarWChar, 0, "sDay"), New System.Data.OleDb.OleDbParameter("sAmount", System.Data.OleDb.OleDbType.Currency, 0, "sAmount")})
+        '
+        'OleDbSelectCommand1
+        '
+        Me.OleDbSelectCommand1.CommandText = "SELECT ID, sYear, sWeek, sDay, sAmount" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "FROM     Sales"
+        '
+        'OleDbUpdateCommand1
+        '
+        Me.OleDbUpdateCommand1.CommandText = resources.GetString("OleDbUpdateCommand1.CommandText")
+        Me.OleDbUpdateCommand1.Parameters.AddRange(New System.Data.OleDb.OleDbParameter() {New System.Data.OleDb.OleDbParameter("sYear", System.Data.OleDb.OleDbType.[Integer], 0, "sYear"), New System.Data.OleDb.OleDbParameter("sWeek", System.Data.OleDb.OleDbType.[Integer], 0, "sWeek"), New System.Data.OleDb.OleDbParameter("sDay", System.Data.OleDb.OleDbType.VarWChar, 0, "sDay"), New System.Data.OleDb.OleDbParameter("sAmount", System.Data.OleDb.OleDbType.Currency, 0, "sAmount"), New System.Data.OleDb.OleDbParameter("Original_ID", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "ID", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_sYear", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "sYear", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_sYear", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "sYear", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_sWeek", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "sWeek", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_sWeek", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "sWeek", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_sDay", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "sDay", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_sDay", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "sDay", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_sAmount", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "sAmount", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_sAmount", System.Data.OleDb.OleDbType.Currency, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "sAmount", System.Data.DataRowVersion.Original, Nothing)})
+        '
+        'OleDbConnection1
+        '
+        Me.OleDbConnection1.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Andrew\Documents\GitHub\For" &
+    "ecaster\Forecaster\Forecaster\ForecasterDB.mdb"
+        '
+        'frmAddSales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(280, 272)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.mskDate)
+        Me.Controls.Add(Me.lblDate)
+        Me.Name = "frmAddSales"
+        Me.Text = "Add Sales"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblDate As Label
+    Friend WithEvents mskDate As MaskedTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents OleDbDataAdapter1 As OleDb.OleDbDataAdapter
+    Friend WithEvents OleDbDeleteCommand1 As OleDb.OleDbCommand
+    Friend WithEvents OleDbInsertCommand1 As OleDb.OleDbCommand
+    Friend WithEvents OleDbSelectCommand1 As OleDb.OleDbCommand
+    Friend WithEvents OleDbUpdateCommand1 As OleDb.OleDbCommand
+    Friend WithEvents OleDbConnection1 As OleDb.OleDbConnection
+End Class
---
+++ Forecaster/Forecaster/addSalesForm.vb
@@ -0,0 +1,43 @@
+﻿Public Class frmAddSales
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim test As Integer = 0
+        Dim saledate As Date = mskDate.Text
+        Dim saleyear As Integer = saledate.Year
+        Dim week_of_year As Double = Math.Ceiling(Convert.ToDouble(saledate.DayOfYear) / 7)
+        Dim saleday As String = saledate.DayOfWeek.ToString.Substring(0, 1)
+        If saledate.DayOfWeek.ToString.Equals("Sunday") Then
+            saleday = "U"
+        End If
+        If saledate.DayOfWeek.ToString.Equals("Thursday") Then
+            saleday = "R"
+        End If
+        Dim amount As Integer = CInt(txtAmount.Text)
+
+        Try
+            OleDbConnection1.Open()
+
+            Dim exists As Integer = 0
+            OleDbDataAdapter1.SelectCommand.Connection = OleDbConnection1
+            OleDbDataAdapter1.SelectCommand.CommandText = "SELECT ID FROM Sales WHERE sYear = " & saleyear & " AND sWeek = " & week_of_year & " AND sDay = '" & saleday & "'"
+            exists = OleDbDataAdapter1.SelectCommand.ExecuteNonQuery()
+            If exists > 0 Then
+                OleDbDataAdapter1.UpdateCommand.Connection = OleDbConnection1
+                OleDbDataAdapter1.UpdateCommand.CommandText = "UPDATE Sales SET sAmount = " & amount & " WHERE sYear = " & saleyear & " AND sWeek = " & week_of_year & " AND sDay = '" & saleday & "'"
+                test = OleDbDataAdapter1.UpdateCommand.ExecuteNonQuery()
+            Else
+                OleDbDataAdapter1.InsertCommand.Connection = OleDbConnection1
+                OleDbDataAdapter1.InsertCommand.CommandText = "INSERT INTO Sales (sYear, sWeek, sDay, sAmount) VALUES (" & saleyear & ", " & week_of_year & ", '" & saleday & "', " & amount & ")"
+                test = OleDbDataAdapter1.InsertCommand.ExecuteNonQuery()
+            End If
+            OleDbConnection1.Close()
+        Catch ex As Exception
+            MessageBox.Show("error has occured; " & ex.Message)
+        End Try
+        If test > 0 Then
+            MessageBox.Show("Your change was saved.")
+        Else
+            MessageBox.Show("Your changes were not saved. (an error has occured)")
+        End If
+
+    End Sub
+End Class
